8d79c006e3422d19c9b35c9579315ab4ce874aee
fanhl <fanhonglin@klicen.com>
2018-05-10 17:56:37 +0800

    change path

--- app/src/main/java/com/fanhl/viewport/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/viewport/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.fanhl.viewport
+package com.fanhl.viewport.ui.main
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.fanhl.viewport.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
